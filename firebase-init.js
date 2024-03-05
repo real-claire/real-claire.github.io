a1238e6f9b7b0232d3ca3e20e5289d2e4120a442
real-claire <clairenoto0@gmail.com>
2024-03-05 06:51:20 -0600

    maybe did something

--- firebase-init.js
+++ firebase-init.js
@@ -1,34 +1,14 @@
-var firebaseConfig = {
+import { initializeApp } from "firebase/app";
+  
+  //nothing to see here...
+  const firebaseConfig = {
     apiKey: "AIzaSyCNg0dn4mTW4E489OkRxwvXAzr25qOMS1M",
-    authDomain: "real-claire.github.io",
+    authDomain: "portfolio-forum-cce27.firebaseapp.com",
     projectId: "portfolio-forum-cce27",
-    storageBucket: "gs://portfolio-forum-cce27",
+    storageBucket: "portfolio-forum-cce27.appspot.com",
     messagingSenderId: "200834829160",
-    appId: "1:200834829160:web:a97c64807874a4aa4c24dd"
+    appId: "1:200834829160:web:a97c64807874a4aa4c24dd",
   };
-  
-  firebase.initializeApp(firebaseConfig);
-  var db = firebase.firestore();
-  
-  // Add a new forum
-  function addForum(title, description) {
-    db.collection("forums").add({
-      title: title,
-      description: description,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp()
-    }).then((docRef) => {
-      console.log("Forum created with ID: ", docRef.id);
-    }).catch((error) => {
-      console.error("Error adding document: ", error);
-    });
-  }
-  
-  // Fetch all forums
-  function fetchForums() {
-    db.collection("forums").orderBy("createdAt").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data().title}`);
-        // Process each forum
-      });
-    });
-  }
+
+  // Initialize Firebase
+  initializeApp(firebaseConfig);
